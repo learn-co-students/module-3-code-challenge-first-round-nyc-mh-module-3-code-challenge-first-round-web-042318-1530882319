c477503f049e09802474118bb183ff901d6169d6
JohnZBurnett <tuninator@gmail.com>
2018-07-06 10:23:32 -0400

    display list and details from class

--- src/beer.js
+++ src/beer.js
@@ -1,4 +1,6 @@
 const beerList = document.getElementById("list-group");
+const beerDetailDiv = document.getElementById("beer-detail");
+
 let id = 0;
 
 class Beer {
@@ -11,7 +13,8 @@ class Beer {
     this.dbId = beerObj.id,
     this.description = beerObj.description,
     this.brewersTips = beerObj.brewers_tips,
-    this.contributedBy = beerObj.contributed_by
+    this.contributedBy = beerObj.contributed_by,
+    this.foodPairings = beerObj.food_pairing
   }
 
   addToList() {
@@ -20,5 +23,21 @@ class Beer {
     beerElement.innerText = this.name;
     beerElement.dataset.beerId = this.id;
     beerList.append(beerElement);
+  }
+
+  renderDetails() {
+    beerDetailDiv.innerHTML = "";
+    beerDetailDiv.innerHTML += `<h1>${this.name}</h1> <img src="${this.imageUrl}"> <h3>${this.tagline}`
+    beerDetailDiv.innerHTML += `<textarea>${this.description}</textarea><button id="edit-beer" class="btn btn-info" data-beer-id="${this.id}">Save</button>`
+    beerDetailDiv.innerHTML += `<h4>First Brewed:</h4> <p>${this.first_brewed}</p>`
+    beerDetailDiv.innerHTML += `<h4>Tips from the Brewers: </h5>${this.brewers_tips}`
+    this.createAndAppendFoodPairings();
+  }
+
+  createAndAppendFoodPairings() {
+    beerDetailDiv.innerHTML += "<h4>Recommended Pairings:</h4>"
+    this.foodPairings.forEach( function(pairing) {
+      beerDetailDiv.innerHTML += `<p>${pairing}</p>`
+    })
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
 const beerURL = "http://localhost:3000/beers"
 document.addEventListener("DOMContentLoaded", function() {
-  let beers;
+  let beers = [];
 
   const beerList = document.getElementById("list-group");
-  const beerDetailDiv = document.getElementById("beer-detail");
   beerList.addEventListener("click", handleDisplayingBeerDetails);
   beerDetailDiv.addEventListener("click", updateBeerDescriptionInDb)
 
-  function createAndAppendBeerElement(beerObj) {
-    let beerElement = document.createElement("li");
-    beerElement.className = "list-group-item";
-    beerElement.innerText = beerObj.name;
-    beerElement.dataset.beerId = beerObj.id;
-    beerList.append(beerElement);
-  }
+  // function createAndAppendBeerElement(beerObj) {
+  //   let beerElement = document.createElement("li");
+  //   beerElement.className = "list-group-item";
+  //   beerElement.innerText = beerObj.name;
+  //   beerElement.dataset.beerId = beerObj.id;
+  //   beerList.append(beerElement);
+  // }
 
   function displayDataFromDb() {
     fetch(beerURL).then( resp => resp.json() ).then( beerJson => handleDisplayingBeer(beerJson) )
   }
 
-  function createAndAppendFoodPairings(beerDetailDiv, beerObj) {
-    beerDetailDiv.innerHTML += "<h4>Recommended Pairings:</h4>"
-    beerObj.food_pairing.forEach( function(pairing) {
-      beerDetailDiv.innerHTML += `<p>${pairing}</p>`
-    })
-  }
+  // function createAndAppendFoodPairings(beerDetailDiv, beerObj) {
+  //   beerDetailDiv.innerHTML += "<h4>Recommended Pairings:</h4>"
+  //   beerObj.food_pairing.forEach( function(pairing) {
+  //     beerDetailDiv.innerHTML += `<p>${pairing}</p>`
+  //   })
+  // }
 
   function fetchBeerDetails(event) {
     let beerId = event.target.dataset.beerId;
@@ -32,8 +31,7 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   function handleDisplayingBeer(beerJson) {
-    beers = beerJson;
-    debugger;
+    beerJson.forEach( beer => beers.push(new Beer(beer)))
     beers.forEach( function(beerObj) {
       beerObj.addToList();
     })
@@ -41,18 +39,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
   function handleDisplayingBeerDetails(event) {
     if (event.target.dataset.beerId) {
-      fetchBeerDetails(event).then(beerObj => renderBeerDetails(beerObj));
+      let currBeer = beers.find( function(beer) { return beer.id === parseInt(event.target.dataset.beerId)} )
+      currBeer.renderDetails(); 
     }
   }
 
-  function renderBeerDetails(beerObj) {
-    beerDetailDiv.innerHTML = "";
-    beerDetailDiv.innerHTML += `<h1>${beerObj.name}</h1> <img src="${beerObj.image_url}"> <h3>${beerObj.tagline}`
-    beerDetailDiv.innerHTML += `<textarea>${beerObj.description}</textarea><button id="edit-beer" class="btn btn-info" data-beer-id="${beerObj.id}">Save</button>`
-    beerDetailDiv.innerHTML += `<h4>First Brewed:</h4> <p>${beerObj.first_brewed}</p>`
-    beerDetailDiv.innerHTML += `<h4>Tips from the Brewers: </h5>${beerObj.brewers_tips}`
-    createAndAppendFoodPairings(beerDetailDiv, beerObj);
-  }
+  // function renderBeerDetails(beerObj) {
+  //   beerDetailDiv.innerHTML = "";
+  //   beerDetailDiv.innerHTML += `<h1>${beerObj.name}</h1> <img src="${beerObj.image_url}"> <h3>${beerObj.tagline}`
+  //   beerDetailDiv.innerHTML += `<textarea>${beerObj.description}</textarea><button id="edit-beer" class="btn btn-info" data-beer-id="${beerObj.id}">Save</button>`
+  //   beerDetailDiv.innerHTML += `<h4>First Brewed:</h4> <p>${beerObj.first_brewed}</p>`
+  //   beerDetailDiv.innerHTML += `<h4>Tips from the Brewers: </h5>${beerObj.brewers_tips}`
+  //   createAndAppendFoodPairings(beerDetailDiv, beerObj);
+  // }
 
   function updateBeerDescriptionInDb(event) {
     if (event.target.id === "edit-beer") {
