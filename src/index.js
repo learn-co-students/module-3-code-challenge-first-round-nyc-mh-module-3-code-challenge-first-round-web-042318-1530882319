aa3e3a983f3a0db1fb14e4ba4a07234ff8b77a83
github username <github email address>
2018-07-06 10:19:01 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 
 const sidebarUl = document.getElementById("list-group")
+const beerDetailDiv = document.getElementById('beer-detail')
 
 document.addEventListener("DOMContentLoaded", function(){
 
@@ -10,22 +11,93 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 
-function manageData(data){
-  displaySidebar(data);
-  //do something with data here
-}
+  function manageData(data){
+    displaySidebar(data);
+  }
 
-function displaySidebar(data){
+  function displaySidebar(data){
 
-    data.forEach(function(individualBeer){
-      let beerName = individualBeer.name
-      let li = document.createElement("li");
-      li.className = "list-group-item"
-      li.innerText = beerName
-      sidebarUl.append(li);
-    })
+      data.forEach(function(individualBeer){
+        let beerName = individualBeer.name;
+        let beerId = individualBeer.id;
+        let li = document.createElement("li");
+        li.className = "list-group-item"
+        li.innerText = beerName
+        sidebarUl.append(li);
+
+        // debugger;
+
+        //add click event to display main detail
+        li.addEventListener("click", function(e){
+          e.preventDefault()//probably not neccessary. Including as precaution.
+          getIndividualBeerData(beerId)
+        })
 
 
-}
+        function getIndividualBeerData(beerId){
+          let url_with_id = `http://localhost:3000/beers/` + beerId;
+          fetch(url_with_id)
+          .then(response=>response.json())
+          .then(json=>showBeerDetail(json));
+        }
+
+          function showBeerDetail(individualBeerData){
+
+
+            let beerHeadline = document.getElementById("detail-beer-name")
+            let beerImage = document.getElementById("detail-beer-image")
+            let beerTagline = document.getElementById("detail-beer-tagline")
+            let beerDescription = document.getElementById("detail-beer-description")
+            let editButton = document.getElementById("edit-beer")
+
+            debugger;
+
+            let individualBeerId = individualBeerData.id;
+            let individualBeerName = individualBeerData.name;
+            let individualBeerImage = individualBeerData.image_url;
+            let individualBeerTagline = individualBeerData.tagline;
+            let individualBeerDescription = individualBeerData.description;
+
+            beerHeadline.innerText = individualBeerName;
+            beerImage.src = individualBeerImage;
+            beerTagline.innerText = individualBeerTagline;
+            beerDescription.innerText = individualBeerDescription;
+
+              editButton.addEventListener("click", function(e){
+                // e.preventDefault();
+                let beerTextField = document.getElementById("detail-beer-description") //difficulty accessing the first time it was assigned
+                let newDescription = beerTextField.value;
+                updateBackend(individualBeerId, newDescription)
+              })
+
+              function updateBackend(beerId, newDescription){
+
+                let url_with_id = `http://localhost:3000/beers/` + beerId;
+                let submissionBody =  {"description": newDescription}
+
+                fetch(url_with_id, {
+                    method: 'PATCH',
+                    headers: {
+                      'Content-Type': 'application/json',
+                      'Accept': 'application/json'
+                    },
+                    body: JSON.stringify(submissionBody)
+                    })
+              }
+
+            }//close showBeerDetail()
+
+
+
+
+
+      })
+  }
+
+
+
+
+
+
 
 })
