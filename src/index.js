8fb42e16ff69a911f3402fdfc740f154ee3b0e3b
JohnZBurnett <tuninator@gmail.com>
2018-07-06 09:57:58 -0400

    display additional beer data

--- src/index.js
+++ src/index.js
@@ -18,6 +18,13 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch(beerURL).then( resp => resp.json() ).then( beerJson => handleDisplayingBeer(beerJson) )
   }
 
+  function createAndAppendFoodPairings(beerDetailDiv, beerObj) {
+    beerDetailDiv.innerHTML += "<h4>Recommended Pairings:</h4>"
+    beerObj.food_pairing.forEach( function(pairing) {
+      beerDetailDiv.innerHTML += `<p>${pairing}</p>`
+    })
+  }
+
   function fetchBeerDetails(event) {
     let beerId = event.target.dataset.beerId;
     return fetch(beerURL + '/' + `${beerId}`).then( resp => resp.json());
@@ -38,9 +45,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
   function renderBeerDetails(beerObj) {
     beerDetailDiv.innerHTML = "";
-    console.log(beerObj);
     beerDetailDiv.innerHTML += `<h1>${beerObj.name}</h1> <img src="${beerObj.image_url}"> <h3>${beerObj.tagline}`
     beerDetailDiv.innerHTML += `<textarea>${beerObj.description}</textarea><button id="edit-beer" class="btn btn-info" data-beer-id="${beerObj.id}">Save</button>`
+    beerDetailDiv.innerHTML += `<h4>First Brewed:</h4> <p>${beerObj.first_brewed}</p>`
+    beerDetailDiv.innerHTML += `<h4>Tips from the Brewers: </h5>${beerObj.brewers_tips}`
+    createAndAppendFoodPairings(beerDetailDiv, beerObj);
   }
 
   function updateBeerDescriptionInDb(event) {
